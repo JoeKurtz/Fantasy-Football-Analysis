ae5d62136ee20e34068321a8c90207253a04f67b
JoeKurtz <jlk660@nyu.edu>
2021-08-27 15:25:46 -0400

    Updated Documents - FFD

---
+++ Fantasy Football Points.py
@@ -0,0 +1,27 @@
+## Offense - Fantasy Football Points Breakdown
+
+
+# Passing
+Point_Pass_Yard = (1/25)
+Point_Pass_TD = 4
+Interception = -2
+
+# Rushing
+Point_Rush_Yard = (1/10)
+Point_Rush_TD = 6
+
+# Receiving
+PPR = 1
+Point_Rec_Yard = (1/10)
+Point_Rec_TD = 6
+
+# Misc. Points
+Point_2PT_Conversion = 2
+Fumble = -2
+Point_Kick_Return = 6
+
+
+print(f"For every Touchdown thrown, your QB gets {Point_Pass_TD} points")
+
+#import numpy as np
+#import pandas as pd
